0ab0a534937274df90316f84669d36d1ee4a0e5e
imrandev20 <imrandev20@gmail.com>
2022-05-26 19:56:50 +0600

    ignored node_modules

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Successfully running server");
+});
